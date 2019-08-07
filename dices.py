1a911307318ff031eea1635161658fe78fac3070
Lokman Bin Khodziri <45467762+LokmanKhodziri@users.noreply.github.com>
2019-08-08 04:13:30 +0800

    Add files via upload

---
+++ dices.py
@@ -0,0 +1,22 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> def main():
+>>> min = 1
+>>> max = 6
+>>> roll_again = "yes"
+>>> while roll_again == "yes" or toll+again == "y"
+>>> print("Rolling the dices...")
+Rolling the dices...
+>>> print("The values are...")
+The values are...
+>>> print(random. randint(min. max))
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(random. randint(min. max))
+AttributeError: 'int' object has no attribute 'max'
+>>> print(random.randint(min, max))
+3
+>>> roll_again + input("Roll the dices again?")
+Roll the dices again?
+'yes'
